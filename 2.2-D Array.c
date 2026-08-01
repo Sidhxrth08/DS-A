d48feb68cf64cbb396dd79db1a0ace9eb0b76081
Sidharth <sid08jn@gmail.com>
2026-08-01 11:35:35 +0530

    Add files via upload

---
+++ 2.2-D Array.c
@@ -0,0 +1,29 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int arr[2][2];
+	int i,j;
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	
+	printf("\n ...Printing array ... \n");
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d \t",arr[i][j]);
+		}
+		
+		printf("\n");
+	}
+	
+getch();
+}
