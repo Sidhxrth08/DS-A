4831cd9e1fcac5f311fe247e8816b83951cc57a9
Sidharth <sid08jn@gmail.com>
2026-08-11 17:03:30 +0530

    Organized DSA files into folder

---
+++ Arrays/1.One-Dimesional Array.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int arr[5];
+	int i;
+	
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		printf("%d    ",arr[i]);
+	}
+	
+getch();
+}
---
+++ Arrays/7.Adding Array element at a specific position.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h> 
+
+int main() {
+    int arr[6] ;
+    int elementToAdd;
+    int i , p;
+    
+    printf("Enter 5 Elements: ");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+	printf("Enter Element position to add(0-5): ");
+    scanf("%d", &p);
+    
+    p -= 1;
+    
+    printf("Enter Element to add: ");
+    scanf("%d", &elementToAdd);
+
+    for (i = 5; i > p; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+
+    arr[p] = elementToAdd;
+
+    
+    printf("\nArray after adding element at: %d \n",p);
+    for (i = 0; i < 6; i++) {
+        printf("Value at index %d is %d\n", i, arr[i]);
+    }
+
+    getch();  
+}
+
+
