0273357217d4141904d2ad91baf1f6d31557e78b
Sidharth <sid08jn@gmail.com>
2026-09-16 20:58:13 +0530

    Add files via upload

---
+++ Arrays/Array Taversal.c
@@ -0,0 +1,28 @@
+#include<conio.h>
+#include<stdio.h>
+void traverse(int arr[], int n){
+    int i;
+    printf("\nArray elements are: ");
+    for(i=0;i<n;i++)
+        printf("%d  ",arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+	int n;
+	
+    printf("\nEnter the size of Array: ");
+    scanf("%d",&n);
+
+    printf("\nEnter %d Elements: \n",n);
+    int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+    traverse(arr,n);
+	
+return 0;
+}
